c7e7ad53df3d8909bb379c98ebcac75f89b8af6f
Christopher M. Larsen <chrislarsen630@u.boisestate.edu>
2020-03-05 12:22:15 -0700

    Eligible cache backend - further cleanup

--- EligibleCacheBackend/DatabaseScripts/InsertTestData.sql
+++ EligibleCacheBackend/DatabaseScripts/InsertTestData.sql
@@ -7,63 +7,69 @@ VALUES ('654321', 'Boulder','Bob');
 INSERT INTO InsuranceProvider (description, provider_npi, test_flag)
 VALUES ('Eligible Test Provider', '1234567893', TRUE);
 
-INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network)
+INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network, plan_level)
 VALUES (
   'United Health Care COMPASS (individual in-network)',
   (SELECT id FROM User WHERE name_last='Andesite'),
   (SELECT id FROM InsuranceProvider WHERE provider_npi='1234567893'),
   '00001',
   'UNITEDHEALTHCARE_COMPASS',
-  'IN'
+  'IN',
+  'INDIVIDUAL'
 );
 
-INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network)
+INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network, plan_level)
 VALUES (
   'Oxford Health (individual in-network)',
   (SELECT id FROM User WHERE name_last='Andesite'),
   (SELECT id FROM InsuranceProvider WHERE provider_npi='1234567893'),
   '00001',
   'OXFORDHEALTH_001',
-  'IN'
+  'IN',
+  'INDIVIDUAL'
 );
 
-INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network)
+INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network, plan_level)
 VALUES (
   'Harvard Pilgrim (individual in-network)',
   (SELECT id FROM User WHERE name_last='Andesite'),
   (SELECT id FROM InsuranceProvider WHERE provider_npi='1234567893'),
   '00001',
   'HARVARDPILGRIM_001',
-  'IN'
+  'IN',
+  'INDIVIDUAL'
 );
 
-INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network)
+INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network, plan_level)
 VALUES (
   'TriCare Standard (individual in-network)',
   (SELECT id FROM User WHERE name_last='Boulder'),
   (SELECT id FROM InsuranceProvider WHERE provider_npi='1234567893'),
   '00001',
   'TRICARE_PLAN_009_TRICARE_STANDARD',
-  'IN'
+  'IN',
+  'INDIVIDUAL'
 );
 
-INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network)
+INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network, plan_level)
 VALUES (
   'Premera (individual in-network)',
   (SELECT id FROM User WHERE name_last='Boulder'),
   (SELECT id FROM InsuranceProvider WHERE provider_npi='1234567893'),
   '00001',
   'PREMERA_001',
-  'IN'
+  'IN',
+  'INDIVIDUAL'
 );
 
-INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network)
+INSERT INTO UserInsurancePlan (description, user_id, provider_id, payer_id, member_id, network, plan_level)
 VALUES (
   'AAK (individual in-network)',
   (SELECT id FROM User WHERE name_last='Boulder'),
   (SELECT id FROM InsuranceProvider WHERE provider_npi='1234567893'),
   '00001',
   'AAK_001',
-  'IN'
+  'IN',
+  'INDIVIDUAL'
 );
 
--- EligibleCacheBackend/ServerRoot/pub/GetInsurancePlanFinancials.php
+++ EligibleCacheBackend/ServerRoot/pub/GetInsurancePlanFinancials.php
@@ -12,7 +12,7 @@
        Error                  If this is present and true, an error was
                               encountered.
 
-       ErrorDescription       Only present if Error is true. Describes the
+       Error_Description      Only present if Error is true. Describes the
                               encountered problem in human-readable format.
 
        Deductible_Remainings  Remaining balance until deductible is met.
@@ -48,7 +48,7 @@ function getAndValidateDatabaseAPIKey(){
 
   $output = new stdClass();
   $output->{'Error'} = true;
-  $output->{'ErrorDescription'} = 'Database API key is not present.';
+  $output->{'Error_Description'} = 'Database API key is not present.';
   echo json_encode($output);
   die();
 }
@@ -61,15 +61,15 @@ function getAndValidateDatabaseAPIKey(){
  *  @retval string Eligible API key. */
 
 function getAndValidateEligibleAPIKey(){
-  if(file_exists('../APIKeys/Eligible.key')){
-    return trim(file_get_contents('../APIKeys/Eligible.key'));
+  if(!file_exists('../APIKeys/Eligible.key')){
+    $output = new stdClass();
+    $output->{'Error'} = true;
+    $output->{'Error_Description'} = 'Eligible API key is not present.';
+    echo json_encode($output);
+    die();
   }
 
-  $output = new stdClass();
-  $output->{'Error'} = true;
-  $output->{'ErrorDescription'} = 'Eligible API key is not present.';
-  echo json_encode($output);
-  die();
+  return trim(file_get_contents('../APIKeys/Eligible.key'));
 }
 
 // _____________________________________________________________________________
@@ -80,15 +80,15 @@ function getAndValidateEligibleAPIKey(){
  *  @retval string User insurance plan ID. */
 
 function getAndValidateParam_UserInsurancePlanID(){
-  if(isset($_GET['uip_id'])){
-    return $_GET['uip_id'];
+  if(!isset($_GET['uip_id'])){
+    $output = new stdClass();
+    $output->{'Error'} = true;
+    $output->{'Error_Description'} = 'User insurance plan ID was not specified.';
+    echo json_encode($output);
+    die();
   }
 
-  $output = new stdClass();
-  $output->{'Error'} = true;
-  $output->{'ErrorDescription'} = 'User insurance plan ID was not specified.';
-  echo json_encode($output);
-  die();
+  return $_GET['uip_id'];
 }
 
 // _____________________________________________________________________________
@@ -109,18 +109,23 @@ function getDatabaseConnection($dbKey){
 
 // _____________________________________________________________________________
 
-/** Queries the database for the plan network type ("in_network"/"out_network").
+/** Queries the database for the plan type. Returns an object containing the
+ *  network ("in_network"/"out_network") and plan level ("INDIVIDUAL"/"FAMILY").
  *
  *  @param  object $db    PDO object for the database connection.
  *  @param  string $uipID User insurance plan ID number.
- *  @retval string Either "in_network" or "out_network" depending on type. */
+ *  @retval object Object containing the plan type. */
 
-function queryDatabase_networkType($db,$uipID){
-  $stmt = $db->prepare("SELECT uip.network FROM UserInsurancePlan AS uip WHERE uip.id = :uip_id;");
-  $stmt->bindParam(':uip_id',$_GET['uip_id']);
+function queryDatabase_getPlanTypeObj($db,$uipID){
+  $stmt = $db->prepare("SELECT uip.network, uip.plan_level FROM UserInsurancePlan AS uip WHERE uip.id = :uip_id;");
+  $stmt->bindParam(':uip_id',$uipID);
   $stmt->execute();
-  $result = $stmt->fetch()['network'];
-  return ($result=='IN' ? 'in_network' : 'out_network');
+  $results = $stmt->fetch();
+
+  $ret = new stdClass();
+  $ret->{'network'   } = ($results['network']=='IN' ? 'in_network' : 'out_network');
+  $ret->{'plan_level'} = $results['plan_level'];
+  return $ret;
 }
 
 // _____________________________________________________________________________
@@ -131,7 +136,7 @@ function queryDatabase_networkType($db,$uipID){
  *  @param  string $uipID User insurance plan ID number.
  *  @return False if the cache is not valid, otherwise an array of cached data. */
 
-function queryDatabase_cachedPlan($db,$uipID){
+function queryDatabase_getCachedPlan($db,$uipID){
   $stmt = $db->prepare("SELECT cached_plan FROM UserInsurancePlan WHERE id = :uip_id AND cached_plan_timestamp > DATE_SUB(NOW(),INTERVAL 1 HOUR);");
   $stmt->bindParam(':uip_id',$uipID);
   $stmt->execute();
@@ -147,7 +152,7 @@ function queryDatabase_cachedPlan($db,$uipID){
  *  @param  string $uipID User insurance plan ID number.
  *  @retval array Array of query fields. */
 
-function queryDatabase_planQueryFields($db,$uipID){
+function queryDatabase_getPlanQueryFields($db,$uipID){
   $stmt = $db->prepare("SELECT ip.provider_npi, ip.test_flag, uip.payer_id, uip.member_id, uip.network FROM UserInsurancePlan AS uip JOIN InsuranceProvider AS ip ON ip.id = uip.provider_id WHERE uip.id = :uip_id;");
   $stmt->bindParam(':uip_id',$uipID);
   $stmt->execute();
@@ -162,7 +167,7 @@ function queryDatabase_planQueryFields($db,$uipID){
  *  @param  array  $queryFields Fields to send in the query.
  *  @retval string JSON object returned by Eligible. */
 
-function queryEligible_planData($eligibleKey,$queryFields){
+function queryEligible_getPlanData($eligibleKey,$queryFields){
   $query  = 'https://gds.eligibleapi.com/v1.5/coverage/all.json?';
   $query .= 'api_key=' . $eligibleKey;
   if($queryFields['test_flag'])$query .= '&test=true';
@@ -198,9 +203,12 @@ function queryDatabase_cachePlanData($db,$uipID,$planData){
 /** Outputs the discovered insurance plans as a JSON object.
  *
  *  @param string $planData Insurance plan data in JSON format.
- *  @param string $networkType Network type, as "in_network" or "out_network". */
+ *  @param object $planTypeObj Object containing the plan network and level. */
 
-function outputPlanData($planData,$networkType){
+function output_planData($planData,$planTypeObj){
+  $networkType = $planTypeObj->{'network'};
+  $planLevel   = $planTypeObj->{'plan_level'};
+
   $output      = new stdClass();
   $planData    = json_decode($planData);
   $deductibleA = $planData->{'plan'}->{'financials'}->{'deductible'};
@@ -208,7 +216,7 @@ function outputPlanData($planData,$networkType){
   $output->{'Deductible_Remainings'} = "0";
   $deductibleA_remainingsA = $deductibleA->{'remainings'}->{$networkType};
   foreach($deductibleA_remainingsA AS $x){
-    if($x->{'level'}=='INDIVIDUAL'){
+    if($x->{'level'}==$planLevel){
       $output->{'Deductible_Remainings'} = $x->{'amount'};
     }
   }
@@ -216,7 +224,7 @@ function outputPlanData($planData,$networkType){
   $output->{'Deductible_Spent'} = "0";
   $deductibleA_spentA = $deductibleA->{'spent'}->{$networkType};
   foreach($deductibleA_spentA AS $x){
-    if($x->{'level'}=='INDIVIDUAL'){
+    if($x->{'level'}==$planLevel){
       $output->{'Deductible_Spent'} = $x->{'amount'};
     }
   }
@@ -224,7 +232,7 @@ function outputPlanData($planData,$networkType){
   $output->{'Deductible_Totals'} = "0";
   $deductibleA_totalsA = $deductibleA->{'totals'}->{$networkType};
   foreach($deductibleA_totalsA AS $x){
-    if($x->{'level'}=='INDIVIDUAL'){
+    if($x->{'level'}==$planLevel){
       $output->{'Deductible_Totals'} = $x->{'amount'};
     }
   }
@@ -242,16 +250,16 @@ function main(){
   $eligibleKey = getAndValidateEligibleAPIKey();
   $uipID       = getAndValidateParam_UserInsurancePlanID();
   $db          = getDatabaseConnection($dbKey);
-  $networkType = queryDatabase_networkType($db,$uipID);
-  $dbCacheData = queryDatabase_cachedPlan ($db,$uipID);
+  $planTypeObj = queryDatabase_getPlanTypeObj($db,$uipID);
+  $dbCacheData = queryDatabase_getCachedPlan ($db,$uipID);
   if($dbCacheData!=false){
     $planData = gzuncompress($dbCacheData['cached_plan']);
   }else{
-    $queryFields = queryDatabase_planQueryFields($db,$uipID);
-    $planData    = queryEligible_planData($eligibleKey,$queryFields);
+    $queryFields = queryDatabase_getPlanQueryFields($db,$uipID);
+    $planData    = queryEligible_getPlanData($eligibleKey,$queryFields);
     queryDatabase_cachePlanData($db,$uipID,$planData);
   }
-  outputPlanData($planData,$networkType);
+  output_planData($planData,$planTypeObj);
 }
 main();
 
